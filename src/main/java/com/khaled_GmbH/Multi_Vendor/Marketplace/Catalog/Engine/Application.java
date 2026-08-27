d3d19053e072d8f19bf0beffffea10e15a38e4bf
Khaled Ahmed <khaled.ahmed@stud-mail.uni-wuerzburg.de>
2026-08-27 16:27:20 +0200

    v1: Database got created with the user table

---
+++ src/main/java/com/khaled_GmbH/Multi_Vendor/Marketplace/Catalog/Engine/Application.java
@@ -0,0 +1,13 @@
+package com.khaled_GmbH.Multi_Vendor.Marketplace.Catalog.Engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/khaled_GmbH/Multi_Vendor/Marketplace/Catalog/Engine/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaled_GmbH.Multi_Vendor.Marketplace.Catalog.Engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
